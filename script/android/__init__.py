8cc93efbdcb2b81547695d107b274baeffdaee3e
Antoine Humbert <antoine.humbert@gmail.com>
2015-08-23 18:08:08 +0200

    Improve acces to adb

---
+++ script/android/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+ADB_PATH = os.path.join(os.path.expanduser('~'),
+                        'AppData', 'Local', 'Android', 'android-sdk', 'platform-tools', 'adb.exe')
--- script/android/push.py
+++ script/android/push.py
@@ -3,11 +3,9 @@ import posixpath
 from pkg_resources import resource_filename
 from subprocess import call
 from script import app_name
+from script.android import ADB_PATH
 
 
-
-ADB_PATH = os.path.join('C:\\', 'Users', 'Antoine', 'AppData', 'Local', 'Android', 'android-sdk', 'platform-tools',
-                        'adb.exe')
 
 if __name__ == '__main__':
     app_dir = os.path.realpath(resource_filename(app_name, '.'))
